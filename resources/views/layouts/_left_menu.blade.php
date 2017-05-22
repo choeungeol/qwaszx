9987b1fa8a932a2a8f66c50bf5eedf91d357cc43
choeungeol <ceg0416@naver.com>
2017-05-22 21:15:32 +0900

    first commit

---
+++ resources/views/layouts/_left_menu.blade.php
@@ -0,0 +1,11 @@
+<div class="col-lg-2">
+    <div class="list-group">
+        <a href="#" class="list-group-item active">
+            menu
+        </a>
+        <a href="#" class="list-group-item">menu 1</a>
+        <a href="#" class="list-group-item">menu 2</a>
+        <a href="#" class="list-group-item">menu 3</a>
+        <a href="#" class="list-group-item">menu 4</a>
+    </div>
+</div>
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts/app')
+
+@section('content')
+	<style>
+		.re {
+			position: relative;
+		}
+	</style>
+	<div class="col-lg-8">
+		<div class="row wri">
+			<div class="panel panel-default">
+				<div class="panel-heading">writer title</div>
+
+				<div class="panel-body">
+					writer
+				</div>
+			</div>
+		</div>
+		@for($i=0;$i<20;$i++)
+		<div class="row re" data-spy="scroll" data-target=".wri">
+			<div class="panel panel-default">
+				<div class="panel-heading">Title {{ $i }}</div>
+
+				<div class="panel-body">
+					body
+				</div>
+			</div>
+		</div>
+		@endfor
+		<div class="row">
+
+		</div>
+	</div>
+@endsection
